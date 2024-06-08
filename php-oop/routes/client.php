e64c2464f73cffb02952a89d2703f083b439b38a
LeDinhHung123 <hungldph45447@fpt.edu.vn>
2024-06-08 22:22:48 +0700

    first

--- php-oop/routes/client.php
+++ php-oop/routes/client.php
@@ -1,5 +1,10 @@
 <?php
 
+use Hungt\PhpOop\Controllers\Client\AuthController;
 use Hungt\PhpOop\Controllers\Client\HomeController;
 
 $router->get( '/', HomeController::class . '@index');
+
+$router->get( '/auth/login',            AuthController::class . '@showFormLogin');
+$router->post( '/auth/handle-login',    AuthController::class . '@login');
+$router->get( '/auth/logout',           AuthController::class . '@logout');
